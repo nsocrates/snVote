f9c253fb9abe7759419e3341294207149e9fd640
Socrates <socratesn@users.noreply.github.com>
2015-08-29 12:47:34 -0700

    add single quotes for string

--- client/app/snAdd/snAdd.directive.js
+++ client/app/snAdd/snAdd.directive.js
@@ -4,12 +4,13 @@ angular.module('testApp')
   .directive('snAdd', function () {
     return {
       restrict: 'EA',
+      link: function (scope, element) {
       	element.bind('click', function() {
       		var el = angular.element(document.getElementById('optionAnchor'));
-      		var form = "<div class='form-group'>"+
-	    					        "<input type='text' name='option' class='form-control form-poll form-option' placeholder='Enter option' required/>"+
-	    					        "<a href='#'><i class='fa fa-times-circle fa-lg'></i></a>"+
-    					       "</div>";
+      		var form = '<div class="form-group">'+
+                        '<input type="text" name="option" class="form-control form-poll form-option" placeholder="Enter option" required/>'+
+                        '<a href="#"><i class="fa fa-times-circle fa-lg"></i></a>'+
+                     '</div>';
           el.append(form);
       		$('.fa-times-circle').on('click', function() {
       			$(this).closest('.form-group').remove();
