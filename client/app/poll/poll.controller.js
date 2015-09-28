ecfbff16760fb3cce541a70a8207ddb4378c91ae
Socrates <nsocrates.dev@gmail.com>
2015-09-28 11:18:08 -0700

    remove bg, make chart responsive

--- client/app/poll/poll.controller.js
+++ client/app/poll/poll.controller.js
@@ -39,6 +39,9 @@ angular.module('testApp')
 		// Callback that creates and populates a data table,
 		// instantiates the pie chart, passes in the data and
 		// draws it.
+		$(window).resize(function(){
+    		drawChart();
+		});
 		function drawChart() {
 
 		// Create the data table.
@@ -52,11 +55,16 @@ angular.module('testApp')
 			colors: ['#F06292', '#BA68C8', '#7986CB', '#9575CD', '#E57373'],
 			fontName: 'Roboto',
 			fontSize: 14,
-			chartArea: {width: '90%', height: '90%'},
 			legend: {alignment: 'center'},
-			width: 400,
-			height: 300,
-			sliceVisibilityThreshold: 0
+			width: '100%',
+			height: '100%',
+			sliceVisibilityThreshold: 0,
+			chartArea: {
+				left: '3%',
+				top: '3%',
+				height: '94%',
+				width: '94%'
+			}
 		};
 
 		// Instantiate and draw our chart, passing in some options.
